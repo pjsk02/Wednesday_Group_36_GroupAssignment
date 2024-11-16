67e05393e4b88fa3184231ad0742134b0dfb818b
pjsk02 <pulipati.j@northeastern.edu>
2024-11-15 19:59:18 -0500

    Making the function to browse product price performance

--- PricingModel/src/main/java/model/ProductManagement/SolutionOffer.java
+++ PricingModel/src/main/java/model/ProductManagement/SolutionOffer.java
@@ -17,6 +17,9 @@ public class SolutionOffer {
     ArrayList<Product> products;
     int price;//floor, ceiling, and target ideas
     MarketChannelAssignment marketchannelcomb;
+    String x;
+    
+    
     public SolutionOffer(MarketChannelAssignment m){
         marketchannelcomb = m;
         products = new ArrayList();
